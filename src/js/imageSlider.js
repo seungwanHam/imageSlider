f8bce66a734ddbc1f55144cfd29dc2aaa0e4ade2
seungwan Ham <seungwan7511@gmail.com>
2022-07-20 23:12:07 +0900

    init webpack + imageSlider JS

---
+++ src/js/imageSlider.js
@@ -0,0 +1,62 @@
+export default class ImageSlider {
+  #currentPosition = 0;
+  #slideNumber = 0;
+  #slideWidth = 0;
+  sliderWrapEl;
+  sliderListEl;
+  nextBtnEl;
+  previousBtnEl;
+
+  constructor() {
+    this.assignElement();
+    this.initSliderNumber();
+    this.initSliderWidth();
+    this.initSliderListWidth();
+    this.addEvent();
+  }
+
+  assignElement() {
+    this.sliderWrapEl = document.getElementById('slider-wrap');
+    this.sliderListEl = this.sliderWrapEl.querySelector('#slider');
+    this.nextBtnEl = this.sliderWrapEl.querySelector('#next');
+    this.previousBtnEl = this.sliderWrapEl.querySelector('#previous');
+  }
+
+  initSliderNumber() {
+    this.#slideNumber = this.sliderListEl.querySelectorAll('li').length;
+  }
+
+  initSliderWidth() {
+    this.#slideWidth = this.sliderListEl.clientWidth;
+  }
+
+  initSliderListWidth() {
+    this.sliderListEl.style.width = `${this.#slideNumber * this.#slideWidth}px`;
+  }
+
+  addEvent() {
+    this.nextBtnEl.addEventListener('click', this.moveToRight.bind(this));
+    this.previousBtnEl.addEventListener('click', this.moveToLeft.bind(this));
+  }
+
+  moveToRight() {
+    // 0 0 / 1 -1000 / 2 -2000  / ... /
+    this.#currentPosition++;
+    if (this.#currentPosition === this.#slideNumber) {
+      this.#currentPosition = 0;
+    }
+    this.sliderListEl.style.left = `-${
+      this.#slideWidth * this.#currentPosition
+    }px`;
+  }
+
+  moveToLeft() {
+    this.#currentPosition--;
+    if (this.#currentPosition === -1) {
+      this.#currentPosition = this.#slideNumber - 1;
+    }
+    this.sliderListEl.style.left = `-${
+      this.#slideWidth * this.#currentPosition
+    }px`;
+  }
+}
